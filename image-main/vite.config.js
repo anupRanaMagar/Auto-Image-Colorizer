9a465d668cdcba57186b631b60bf101d0d2be3ca
apple <apple@apples-MacBook-Air.local>
2024-05-22 00:18:50 +0545

    small change 4

--- image-main/vite.config.js
+++ image-main/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react' //removed swc
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: "staticfiles_build",
+    }
 })
